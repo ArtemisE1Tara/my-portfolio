7c44cdde206ca56f8d93e0bed80ffd28389908e5
ArtemisE1Tara <ahmedsecen2@gmail.com>
2024-10-23 23:47:42 -0400

    fb

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Navbar } from "@/components/NavBar";
+import { Navbar } from "@/components/Navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 
